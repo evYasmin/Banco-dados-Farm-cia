58759057484f9eca2f6f18ac85e5557f35af698e
evYasmin <49412927+evYasmin@users.noreply.github.com>
2019-06-07 14:36:50 -0300

    Feito o model de Comestiveis

---
+++ BancoDadosFarmacia/Model/Comestiveis.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Comestiveis
+    {
+        public int id;
+        public string nome;
+        public decimal valor;
+        public DateTime dataVencimento;
+        public int quantidade;
+        public string marca;
+    }
+}
